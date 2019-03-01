2874e2872cdbd4874362a1fb9337db9f53b43a5e
tansioooooooo <tansioooooooo@gmail.com>
2019-03-01 13:28:15 +0900

    file name change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    
+    render template "users/mypage"
   end
 
   def show
@@ -9,18 +9,22 @@ class UsersController < ApplicationController
   end
   
   def new
-
+    render template "users/identification"
   end
   
   def create
-
+    render template "users/"
   end
   
   def edit
-
+    render template "users/profile"
   end
 
   def update
 
+  end
+
+  def delete
+    render template "users/logout"
   end
 end
