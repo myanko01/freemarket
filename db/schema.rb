cd4f591e6c83a1aa9a4cd1e8bdd2a7cf648fc50a
tansioooooooo <tansioooooooo@gmail.com>
2019-03-04 15:53:08 +0900

    add model shipping_date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_065011) do
+ActiveRecord::Schema.define(version: 2019_03_04_065210) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "zip", limit: 7
@@ -77,6 +77,12 @@ ActiveRecord::Schema.define(version: 2019_03_04_065011) do
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_sales_on_item_id"
     t.index ["user_id"], name: "index_sales_on_user_id"
+  end
+
+  create_table "shipping_dates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
