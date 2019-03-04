03685e43768eb120ef0c29ccc99c5bfdfa7dfb95
tansioooooooo <tansioooooooo@gmail.com>
2019-03-04 14:53:24 +0900

    migrate sales

--- db/migrate/20190304015132_create_sales.rb
+++ db/migrate/20190304015132_create_sales.rb
@@ -1,8 +1,8 @@
 class CreateSales < ActiveRecord::Migration[5.2]
   def change
     create_table :sales do |t|
-      t.reference :user, null: false, foreign_key:true
-      t.reference :item, null: false, foreign_key:true
+      t.references :user, null: false
+      t.references :item, null: false
       t.integer :status, null: false, default: 0
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_014735) do
+ActiveRecord::Schema.define(version: 2019_03_04_015132) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "zip", limit: 7
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 2019_03_04_014735) do
     t.string "building"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sales", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.integer "status", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_sales_on_item_id"
+    t.index ["user_id"], name: "index_sales_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
