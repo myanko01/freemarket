0fe80538019860b52871f312223970807e9ba0ae
sakurannn <hpns.x5@gmail.com>
2019-03-12 11:48:00 +0900

    アソシエーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :brand
-  belongs_to :ship_from
   belongs_to :shipping_date
   belongs_to :condition
   has_many :images
+  belongs_to :category
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 end
