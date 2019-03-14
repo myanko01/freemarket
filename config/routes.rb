22c589caaf8bff2e6fb81d37722ddd49df05d61e
tansioooooooo <tansioooooooo@gmail.com>
2019-03-14 20:09:14 +0900

    add address routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'items#index'
   resources :items do
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :categories, only: [:index, :show]
   resources :users, except: [:index, :destroy] do
     resources :cards, only: [:index, :new, :create, :destroy]
+    resources :addresses, only: [:new, :create, :edit, :update]
   end
   resources :brands, only: [:index, :show]
 end
