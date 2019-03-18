5b432efd611da9e2a466e6b9d34795d38b4f29d0
myanko01 <yuuka.nenneco01@gmail.com>
2019-03-18 16:38:36 +0900

    出品実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  belongs_to :brand
+  belongs_to :brand, optional: true
   has_many :images
   accepts_nested_attributes_for :images
   belongs_to :category
