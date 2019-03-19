a4c48c8e2e18ae32e09b76f73b2c38683f3f60bf
tansioooooooo <tansioooooooo@gmail.com>
2019-03-19 17:21:19 +0900

    fix user

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,8 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
       user.email = auth.info.email
       user.nickname = auth.info.name
+      user.uid = auth.uid
+      user.provider = auth.provider
     end
-  end
-
-  def self.set_password
-    Devise.friendly_token[0,20]
   end
 end
