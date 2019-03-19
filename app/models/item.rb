e12f74a7aad0e1c6c06ca283ff997154e3fa14fc
notkato <tennisnaoto@gmail.com>
2019-03-19 15:26:52 +0900

    image update JS etc

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :brand
   has_many :images
-  accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to :category
 
   extend ActiveHash::Associations::ActiveRecordExtensions
