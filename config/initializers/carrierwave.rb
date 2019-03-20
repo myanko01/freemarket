b5ad8e9a4f0824ea2d89960c67b9f9f6e81c608c
tansioooooooo <36565362+tansioooooooo@users.noreply.github.com>
2019-03-20 21:50:23 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,19 +2,16 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
- CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test? #追記部分
-    config.storage = :file
-  elsif Rails.env.production?
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-      region: 'ap-northeast-1'
-    }
-  end
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+    region: 'ap-northeast-1'
+  }
+end
 
   config.fog_directory  = 'freemarket-45a'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-45a'
