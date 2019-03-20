e10a30cefcb35117a1d73890cd127acdfff5e64f
tansioooooooo <36565362+tansioooooooo@users.noreply.github.com>
2019-03-20 21:51:12 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,7 +11,6 @@ CarrierWave.configure do |config|
     aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
     region: 'ap-northeast-1'
   }
-end
 
   config.fog_directory  = 'freemarket-45a'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-45a'
